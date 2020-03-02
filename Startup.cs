d620eb8e378b7feab1d87e665f948a7763d6136a
MinGyu, Ju <knighp@gmail.com>
2020-03-03 00:11:23 +0900

    First Commit
    First Commit

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace pipelines_dotnet_core
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30? 31 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
